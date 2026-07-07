7252d10acd11ebe837b49c98d2c8d39e3d3cb496
rsod amoeba <160609079+swamprsod@users.noreply.github.com>
2026-07-07 10:18:56 +0700

    Add files via upload

---
+++ c.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+#define b 60
+
+int a=0;
+int m=((b/2)-2);
+void clear(){
+system("clear");
+}
+
+void fih(){
+a=0;
+m=((b/2)-2);
+while(a!=b){
+	if(a<=(b/2)){
+		for(int i=0; i<a;){
+			printf(" ");
+			i++;
+		}
+		clear();
+		printf("><>\n");
+		a++;
+		usleep(100000);
+	}
+	else{
+		for(int j=0; j<m;){
+			printf(" ");
+			j++;
+		}
+		clear();
+		m--;
+		printf("<><\n");
+		a++;
+		usleep(100000);	
+	}
+}
+}
+int
+main(){
+
+
+for(int i=0;i<5;){
+fih();
+i++;
+}
+}
